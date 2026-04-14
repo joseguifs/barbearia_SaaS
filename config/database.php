e6b70199acc4ef0719a1ee1a74e0961cf96205ca
jcarlos721 <jcarlos721254@gmail.com>
2026-04-13 22:25:34 -0300

    corrigindo rotas

--- config/database.php
+++ config/database.php
@@ -1,14 +1,13 @@
 <?php
-$host = "localhost";
-$db = "barbertime";
-$user = "root";
-$pass = "";
+
+$host = 'localhost';
+$db = 'nome_do_seu_banco';
+$user = 'root';
+$pass = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
+    die("Erro de conexão: " . $e->getMessage());
 }
-?>
--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
+// Conexão com banco
+require_once __DIR__ . '/../config/database.php';
+
+// Controllers
 require_once __DIR__ . '/../app/controllers/AdminController.php';
 
+// Captura da ação
+$action = $_GET['action'] ?? '';
+
+// Instancia controller
 $controller = new AdminController($pdo);
 
+// Rotas
 switch ($action) {
 
     case 'review_pending':
@@ -16,5 +25,9 @@ switch ($action) {
 
     case 'review_reject':
         $controller->reject();
+        break;
+
+    default:
+        echo "Rota inválida";
         break;
 }
