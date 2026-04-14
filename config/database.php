43a2a300e3b328d0782d0ffcb1b84f841e89e12e
jcarlos721 <jcarlos721254@gmail.com>
2026-04-13 22:28:39 -0300

    correção conexão com o banco

--- config/database.php
+++ config/database.php
@@ -1,13 +1,14 @@
 <?php
-
-$host = 'localhost';
-$db = 'barbertime';
-$user = 'root';
-$pass = '';
+$host = "localhost";
+$db = "barbertime";
+$user = "root";
+$pass = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
-    die("Erro de conexão: " . $e->getMessage());
+    echo "Erro na conexão: " . $e->getMessage();
 }
+?>
