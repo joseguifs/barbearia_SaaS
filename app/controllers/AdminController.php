1e557449d121a823ce92efbfa99e219d3af5d34c
jcarlos721 <jcarlos721254@gmail.com>
2026-04-13 22:11:35 -0300

    ajuste na feature

--- app/controllers/AdminController.php
+++
@@ -1,9 +0,0 @@
-public function pending()
-{
-    require_once __DIR__ . '/../models/Scheduling.php';
-
-    $model = new Scheduling($this->pdo);
-    $agendamentos = $model->getPending();
-
-    require __DIR__ . '/../views/admin/pending.php';
-}
