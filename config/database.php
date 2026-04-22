c78b2b1bb93da2fd1322eeca05a5eb44e77fd559
joseguifs <joseguilhermeagente007@gmail.com>
2026-04-21 22:07:07 -0300

    Revertendo para o estado do commit 9c26bee

--- config/database.php
+++ config/database.php
@@ -1,14 +1,14 @@
 <?php
-
 $host = "localhost";
-$db = "barbertime"; 
+$db = "barbertime";
 $user = "root";
-$pass = "jose2005";
+$pass = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+    echo "Erro na conexão: " . $e->getMessage();
 }
+?>
