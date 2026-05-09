402965b97b9b82771957d1eb5bea4c81cbbdb65e
joseguifs <joseguilhermeagente007@gmail.com>
2026-05-09 19:33:23 -0300

    Api de autenticacao concluida

--- app/APIs/AuthApi.php
+++ app/APIs/AuthApi.php
@@ -1,29 +1,15 @@
 <?php
-
+require_once __DIR__ . '/BaseApi.php';
 require_once __DIR__ . '/../models/Client.php';
 
-class AuthApi
+
+class AuthApi extends BaseApi
 {
     private $clientModel;
 
     public function __construct($pdo)
     {
         $this->clientModel = new Client($pdo);
-    }
-
-    private function startSessionIfNeeded()
-    {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-    }
-
-    private function json($data, int $statusCode = 200)
-    {
-        http_response_code($statusCode);
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
-        exit;
     }
 
     public function login()
