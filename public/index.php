710e450458aa2ca53278bfb14bc970b50abf4436
Jose Carlos <jc721254@gmail.com>
2026-05-26 09:02:11 -0300

    Ajuste das rotas na index

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ require_once __DIR__ . '/../app/controllers/AuthController.php';
 require_once __DIR__ . '/../app/controllers/UserController.php';
 require_once __DIR__ . '/../app/controllers/SchedulingReviewController.php';
 require_once __DIR__ . '/../app/controllers/SchedulingController.php';
+require_once __DIR__ . '/../app/controllers/BarberController.php';
+
+// Junto com as outras instâncias, adicione:
+$barberController = new BarberController($pdo);
 
 // Junto com as outras instâncias, adicione:
 $barberController = new BarberController($pdo);
@@ -90,6 +94,10 @@ switch ($action) {
 
     case 'review_reject':
         $reviewController->reject();
+        break;
+        
+    case 'barber_profile':
+        $barberController->profile();
         break;
 
     default:
