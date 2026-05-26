6bca2e292c258cd19aa107f8eee6a87cd9559270
joseguifs <joseguilhermeagente007@gmail.com>
2026-05-26 20:25:18 -0300

    Delete tests/run.php

--- tests/run.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "==============================" . PHP_EOL;
-echo " TESTES NATIVOS - BARBERTIME" . PHP_EOL;
-echo "==============================" . PHP_EOL . PHP_EOL;
-
-require_once __DIR__ . '/Unit/BarberTest.php';
-
-echo PHP_EOL . "Testes finalizados." . PHP_EOL;
