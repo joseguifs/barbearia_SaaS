1d652c9896f207f05b4c88cb8588a84388282e34
joseguifs <joseguilhermeagente007@gmail.com>
2026-06-16 19:16:35 -0300

    ajuste na listagem de agendamento

--- app/views/scheduling/list.php
+++ app/views/scheduling/list.php
@@ -215,6 +215,7 @@ $clienteInicial = function_exists('mb_substr')
             <a href="index.php?action=home">Início</a>
             <a href="index.php?action=scheduling_create">Agendar</a>
             <a href="index.php?action=scheduling_list" class="active">Meus Agendamentos</a>
+            <a href="index.php?action=scheduling_history">Histórico</a>
             <a href="index.php?action=logout" class="btn-logout">Sair</a>
         </nav>
     </header>
